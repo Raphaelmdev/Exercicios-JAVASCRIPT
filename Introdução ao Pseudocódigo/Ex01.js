5def9a8e93be6c88a702a1dcfe341ac1db50056c
Raphael Martins <137070545+Raphaelmdev@users.noreply.github.com>
2026-03-06 00:04:17 -0300

    Introdução ao Pseudocódigo  Condicionais  EXERCÍCIOS REFERENTES AO CURSO DE FRONT-END SENAI

--- Introdução ao Pseudocódigo/Ex01.js
+++ Introdução ao Pseudocódigo/Ex01.js
@@ -1,6 +1,5 @@
-const prompt = require("prompt-sync")();
+let valor = Number(prompt("Digite um valor:"));
 
-let valor = Number(prompt("Digite um valor: "));
 let antecessor = valor - 1;
 
 console.log("O antecessor de", valor, "é", antecessor);
